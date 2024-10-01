c7cdbb1e16ef761eecc9d706bb04c00d07710bba
fromtheeast710 <theeast710@proton.me>
2024-10-02 01:23:37 +1000

    Feat: Create module vars

---
+++ bin/vars.ml
@@ -0,0 +1,20 @@
+let width = 800
+let height = 450
+let is_show_help = false
+
+let help =
+  "*** Keybinds *** \n\
+   h: view all commands \n\
+   w: weight \n\
+   v: velocity \n\
+   a: trail \n\
+   x: axis \n\
+   g: grid \n\
+   c: connection \n\
+   s: colorscale \n\
+   t: time \n\
+   f: fps \n\
+   space: pause/start"
+;;
+
+let helper = "Press h to view all commands."
